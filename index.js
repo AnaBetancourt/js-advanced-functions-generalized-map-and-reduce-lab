96c34d092c392166968573775b7509616da9107b
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-04-12 21:25:22 -0400

    commit tree 4a565661bafd4f4bf0b367b3396899341bc41fde

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+function map(array, funct){
+    let newArray = []
+    for (let i = 0; i < array.length; i++){
+        newArray.push(funct(array[i]))
+    }
+    return newArray
+}
+
+function reduce(array, funct, startingValue){
+    let start = (!!startingValue) ? startingValue : array[0]
+    let i = (!!startingValue) ? 0 : 1
+    for (; i < array.length; i++){
+        start = funct(array[i], start)
+    }
+    return start
+}
